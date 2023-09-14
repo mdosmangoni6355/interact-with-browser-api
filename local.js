c3ce1251c1889f36bfa7413885670d27ac06b292
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-09-14 19:34:50 +0600

    some file added

---
+++ local.js
@@ -0,0 +1,15 @@
+const hello = localStorage.getItem('hello')
+console.log(hello)
+
+const getLocal = () =>{
+    window.localStorage.setItem("name: Osman", "age: 22");
+}
+
+// localStorage.getItem()
+// localStorage.setItem()
+// localStorage.removeItem
+// localStorage.clear
+// toString()
+// JSON.stringify()
+// typeof
+// JSON.parse()
